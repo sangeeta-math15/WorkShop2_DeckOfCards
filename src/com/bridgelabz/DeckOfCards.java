53c011bc598922e724cae6edb18f33278fc4e5e0
sngtmth <sngtmth@gmail.com>
2021-10-13 09:46:06 +0530

    Added:Initialize Cards

--- src/com/bridgelabz/DeckOfCards.java
+++ src/com/bridgelabz/DeckOfCards.java
@@ -9,10 +9,19 @@ package com.bridgelabz;
  */
 
 public class DeckOfCards {
-
+	//Driver code
 	public static void main(String[] args) {
 		System.out.println("Welcome to Deck Of Card Problem");
 
-	}
+		String[] deck = new String[52];
+		String[] suits = { "Club", "Diamond", "Heart", "Spade" };
+		String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Ace", "King", "Queen", "Jack" };
+		int pos = 0;
+		for (int i = 0; i < suits.length; i++) {
+			for (int j = 0; j < ranks.length; j++) {
+				deck[pos] = suits[i] + ranks[j];
 
+			}
+		}
+	}
 }
