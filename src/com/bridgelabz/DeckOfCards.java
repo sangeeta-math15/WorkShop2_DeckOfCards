18e480b2757e67162ed90a23ecb2da751b818018
sngtmth <sngtmth@gmail.com>
2021-10-13 08:59:26 +0530

    Added:Welcome Message

---
+++ src/com/bridgelabz/DeckOfCards.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+/**
+ * 
+ * DeckOfCards Program
+ * 
+ * @author Sangeeta Math
+ *
+ */
+
+public class DeckOfCards {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Deck Of Card Problem");
+
+	}
+
+}
